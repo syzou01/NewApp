c6853a04181bc064de184bd1d852f6705ebc11de
Siyi Zou <syzou01@gmail.com>
2020-06-05 07:52:43 +0800

    Score count app for 2 teams

--- app/src/main/java/com/swufe/newapp/ScoreCountActivity.java
+++ app/src/main/java/com/swufe/newapp/ScoreCountActivity.java
@@ -16,24 +16,42 @@ public class ScoreCountActivity extends AppCompatActivity {
 
 
     public void btn1(View v) {
-        show(1);
+        if(v.getId()==R.id.btn1a)
+            show1(1);
+        else
+            show2(1);
     }
 
     public void btn2(View v) {
-        show(2);
+        if(v.getId()==R.id.btn1a)
+            show1(2);
+        else
+            show2(2);
     }
 
     public void btn3(View v) {
-        show(3);
+        if(v.getId()==R.id.btn1a)
+            show1(3);
+        else
+            show2(3);
     }
 
     public void btnReset(View v) {
-        TextView out = (TextView)findViewById(R.id.score);
+        TextView out = (TextView)findViewById(R.id.score1);
+        out.setText("0");
+        out = (TextView)findViewById(R.id.score2);
         out.setText("0");
     }
 
-    private void show(int i){
-        TextView out = (TextView)findViewById(R.id.score);
+    private void show1(int i){
+        TextView out = (TextView)findViewById(R.id.score1);
+        String oldScore = (String) out.getText();
+        String newScore = String.valueOf(Integer.parseInt(oldScore) + i);
+        out.setText(newScore);
+    }
+
+    private void show2(int i){
+        TextView out = (TextView)findViewById(R.id.score2);
         String oldScore = (String) out.getText();
         String newScore = String.valueOf(Integer.parseInt(oldScore) + i);
         out.setText(newScore);
