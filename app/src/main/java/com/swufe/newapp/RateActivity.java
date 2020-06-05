a6c5dea8370760eea225c5b6e0447ff7e82aaeed
Siyi Zou <syzou01@gmail.com>
2020-06-05 19:30:27 +0800

    获取网络数据

--- app/src/main/java/com/swufe/newapp/RateActivity.java
+++ app/src/main/java/com/swufe/newapp/RateActivity.java
@@ -19,6 +19,13 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.DecimalFormat;
 
 public class RateActivity extends AppCompatActivity implements View.OnClickListener,Runnable {
@@ -168,5 +175,34 @@ public class RateActivity extends AppCompatActivity implements View.OnClickListe
         //msg.obj = "Hello from run()";
         msg.obj = "HELLO!";
         handler.sendMessage(msg);
+
+        URL url = null;
+        try {
+            url = new URL("http://www.usd-cny.com/icbc.htm");
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            InputStream in = http.getInputStream();
+
+            String html = inputStream2String(in);
+            Log.i(TAG, "run: html=" + html);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String inputStream2String(InputStream inputStream) throws IOException {
+        final int bufferSize = 1024;
+        final char[] buffer = new char[bufferSize];
+        final StringBuilder out = new StringBuilder();
+        Reader in = new InputStreamReader(inputStream, "gb2312");
+        while (true) {
+            int rsz = in.read(buffer, 0, buffer.length);
+            if (rsz < 0)
+                break;
+            out.append(buffer, 0, rsz);
+        }
+        return out.toString();
     }
 }
