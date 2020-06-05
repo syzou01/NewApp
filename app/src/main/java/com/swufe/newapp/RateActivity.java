b88df555b68881e866342b1d7ad4a5382e05e78a
Siyi Zou <syzou01@gmail.com>
2020-06-05 11:58:24 +0800

    简单汇率换算

---
+++ app/src/main/java/com/swufe/newapp/RateActivity.java
@@ -0,0 +1,59 @@
+package com.swufe.newapp;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RateActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mTextView;
+    private final String TAG = "Rate";
+    private float dollarRate = 0.1f;
+    private float euroRate = 0.2f;
+    private float wonRate = 0.3f;
+    EditText rmb;
+    TextView show;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate);
+
+        rmb = (EditText) findViewById(R.id.rmb);
+        show = (TextView) findViewById(R.id.show);
+
+
+    }
+
+    @Override
+    public void onClick(View btn) {
+        Log.i(TAG, "onClick: ");
+        String str = rmb.getText().toString();
+        Log.i(TAG, "onClick: get str=" + str);
+
+        float r = 0;
+        if(str.length()>0){
+            r = Float.parseFloat(str);
+        }else{
+            //用户没有输入内容
+            Toast.makeText(this, "请输入内容", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Log.i(TAG, "onClick: r=" + r);
+
+        //计算
+        if(btn.getId()==R.id.dollar){
+            show.setText(String.valueOf(r*dollarRate));
+        }else if(btn.getId()==R.id.euro){
+            show.setText(String.valueOf(r*euroRate));
+        }else{
+            show.setText(String.valueOf(r*wonRate));
+        }
+    }
+}
